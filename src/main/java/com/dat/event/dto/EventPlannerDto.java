878c2cf602b42711e9cd2346bea27e0a509c7a44
25-00294 <25-00294@5e73b0b3-88fa-41e2-a44a-e6aac4854029>
2025-08-11 21:00:34 +0000

    mma
    git-svn-id: http://10.1.8.14:18080/svn/Local_Business/DAT-Event/Club-Event@3531 5e73b0b3-88fa-41e2-a44a-e6aac4854029

--- src/main/java/com/dat/event/dto/EventPlannerDto.java
+++ src/main/java/com/dat/event/dto/EventPlannerDto.java
@@ -1,12 +1,15 @@
 package com.dat.event.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class EventPlannerDto {
 
     private Long staffId;
     private Long eventId;
+    private String supportedMonth;
     private Boolean supportedMemberFlg;
     private Boolean delFlg;
 
