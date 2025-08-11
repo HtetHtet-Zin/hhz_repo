b1f36d74551f7c2c9086048d9020ef7cd8ec04c7
25-00321 <25-00321@5e73b0b3-88fa-41e2-a44a-e6aac4854029>
2025-08-11 00:50:07 +0000

    KKK - session kill
    git-svn-id: http://10.1.8.14:18080/svn/Local_Business/DAT-Event/Club-Event@3517 5e73b0b3-88fa-41e2-a44a-e6aac4854029

---
+++ src/main/java/com/dat/event/config/SessionCleanupOnShutdown.java
@@ -0,0 +1,43 @@
+package com.dat.event.config;
+
+import jakarta.annotation.PreDestroy;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionEvent;
+import jakarta.servlet.http.HttpSessionListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+@Component
+public class SessionCleanupOnShutdown implements HttpSessionListener {
+
+    private static final Set<HttpSession> sessions =
+            Collections.synchronizedSet(new HashSet<>());
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        sessions.add(se.getSession());
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        sessions.remove(se.getSession());
+    }
+
+    @PreDestroy
+    public void invalidateAllSessions() {
+        for (HttpSession session : sessions ) {
+            try {
+                session.invalidate();
+            } catch (IllegalStateException ignored) {
+                // Already invalidated
+                log.info("Already invalidated");
+            }
+        }
+        log.info("All active HTTP sessions invalidated on shutdown.");
+    }
+}
