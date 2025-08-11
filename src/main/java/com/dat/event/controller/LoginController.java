d20db0fd0cef9b93e0edc5fed44dee0a11f642e8
25-00321 <25-00321@5e73b0b3-88fa-41e2-a44a-e6aac4854029>
2025-08-11 00:26:54 +0000

    KKK - 404
    git-svn-id: http://10.1.8.14:18080/svn/Local_Business/DAT-Event/Club-Event@3516 5e73b0b3-88fa-41e2-a44a-e6aac4854029

--- src/main/java/com/dat/event/controller/LoginController.java
+++ src/main/java/com/dat/event/controller/LoginController.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -53,7 +52,10 @@ public class LoginController {
     }
 
     @GetMapping(WebUrl.ERROR_URL)
-    public String ErrorPage() {
+    public String ErrorPage(HttpSession session) {
+        if (session != null && session.getAttribute("staffNo") != null) {
+            session.invalidate();
+        }
         return "404";
     }
 
