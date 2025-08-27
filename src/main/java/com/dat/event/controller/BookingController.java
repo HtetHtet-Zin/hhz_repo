9b8648d5464b709561f650c927f108bee5b316f4
25-00300 <25-00300@5e73b0b3-88fa-41e2-a44a-e6aac4854029>
2025-08-27 02:02:45 +0000

    git-svn-id: http://10.1.8.14:18080/svn/Local_Business/DAT-Event/Club-Event@3749 5e73b0b3-88fa-41e2-a44a-e6aac4854029

---
+++ src/main/java/com/dat/event/controller/BookingController.java
@@ -0,0 +1,24 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.dat.event.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+/**
+ * BookingController Class.
+ * <p>
+ * </p>
+ *
+ * @author kyi min khant
+ */
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class BookingController {
+}
