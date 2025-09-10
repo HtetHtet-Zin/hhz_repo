5cdee1220289b55d04ca6b253f2f71099422a606
25-00294 <25-00294@5e73b0b3-88fa-41e2-a44a-e6aac4854029>
2025-09-10 01:43:12 +0000

    fix mail template style with html
    git-svn-id: http://10.1.8.14:18080/svn/Local_Business/DAT-Event/Club-Event@3847 5e73b0b3-88fa-41e2-a44a-e6aac4854029

--- src/main/java/com/dat/event/email/service/EmailService.java
+++ src/main/java/com/dat/event/email/service/EmailService.java
@@ -4,14 +4,9 @@ import com.dat.event.email.config.EmailProperties;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Properties;
 
 @Service
 @RequiredArgsConstructor
@@ -27,7 +22,7 @@ public class EmailService {
             helper.setFrom(userEmail);
             helper.setTo(recipient);
             helper.setSubject(subject);
-            helper.setText(body, false);
+            helper.setText(body, true);
             mailSender.send(message);
         }
     }
