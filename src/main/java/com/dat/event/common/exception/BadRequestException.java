cb6cf36da3b1e6c8a4ac7d647e086e3c19b0e9e3
25-00329 <25-00329@5e73b0b3-88fa-41e2-a44a-e6aac4854029>
2025-10-27 21:11:59 +0000

    Add BadRequestException.java
    git-svn-id: http://10.1.8.14:18080/svn/Local_Business/DAT-Event/Club-Event@3957 5e73b0b3-88fa-41e2-a44a-e6aac4854029

---
+++ src/main/java/com/dat/event/common/exception/BadRequestException.java
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.dat.event.common.exception;
+
+/**
+ * BadRequestException Class.
+ * <p>
+ * </p>
+ *
+ * @author Zwel Naing Oo
+ */
+
+public class BadRequestException extends RuntimeException{
+    public BadRequestException (String message){
+        super(message);
+    }
+}
